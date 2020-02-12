5a2e4e0197eaf6e2312a7779239a2bbd42595acd
Liz Daly <liz7daly@gmail.com>
2020-02-12 15:24:48 +0000

    Add add bookmarks form

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -3,11 +3,22 @@ require_relative '../lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
  
 
   get '/' do
     @bookmarks = Bookmark.all
+    @url = session[:url]
     erb :index
+  end
+
+  get '/add' do
+    erb :add
+  end
+
+  post '/add' do
+    session[:url] = params[:url]
+    redirect '/'
   end
 
 end

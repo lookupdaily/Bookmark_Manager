fababeaf934ae38a4afbe147f0c9ae4530f6f1de
Liz Daly <liz7daly@gmail.com>
2020-02-14 10:44:26 +0000

    Update tests to use wrapped data
    Bookmark previously only wrapped returned data, but we now wrap data on adding
    to the database as well.

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -29,7 +29,8 @@ class Bookmark
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-    connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}')RETURNING id, url, title")
+    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}')RETURNING id, url, title")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url']) 
   end 
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,9 +4,9 @@ describe Bookmark do
 
   describe '.create' do
     it 'creates a new bookmark' do
-      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark').first
-      expect(bookmark['url']).to eq 'http://www.testbookmark.com'
-      expect(bookmark['title']).to eq 'Test Bookmark'
+      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+      expect(bookmark.url).to eq 'http://www.testbookmark.com'
+      expect(bookmark.title).to eq 'Test Bookmark'
     end
   end
   
@@ -23,7 +23,7 @@ describe Bookmark do
    
       expect(bookmarks.length).to eq 3
       expect(bookmarks.first).to be_a Bookmark
-      expect(bookmarks.first.id).to eq bookmark.first.id
+      expect(bookmarks.first.id).to eq bookmark.id
       expect(bookmarks.first.title).to eq 'Makers Academy'
       expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
      end

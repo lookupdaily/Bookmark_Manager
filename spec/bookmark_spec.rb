12165dbe98833e2b080aeb0832db92ddddb1e2bb
Liz Daly <liz7daly@gmail.com>
2020-02-15 11:12:25 +0000

    Remove useless line in bookmark_spec
    Rubocop pulled up a useless variable that should have been refactored out at an
    earlier stage.

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -12,9 +12,6 @@ describe Bookmark do
   
   describe '.all' do
     it 'returns a list of bookmarks' do
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-
-      # Add the test data
       bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
       Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
       Bookmark.create(url: "http://www.google.com", title: "Google")
